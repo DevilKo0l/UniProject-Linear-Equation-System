ae50f9ee481c69b814afee6d953b90ba36bc298e
Trung Nguyen <nguyen.tcao96@gmail.com>
2020-01-18 11:05:00 +0100

    Refactoring code: add class EquationSystem, GausianElimination and Matrix

---
+++ EquationSystem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Equation_System
+{
+    class EquationSystem: Matrix
+    {
+        private readonly GaussianElimination _gausian;        
+        
+        public EquationSystem(int newNumberOfEquation, int newNumberOfUnknown, GaussianElimination gaussian)
+            :base(newNumberOfEquation, newNumberOfUnknown)
+        {
+            _gausian = gaussian;
+           
+        }
+
+        public void DisplayEquation()
+        {
+            Console.Clear();            
+            string[] variableArray = Enumerable.Repeat("X", nCol).ToArray();
+            Console.WriteLine("\nYou have entered the following equation:");
+            for (int i = 0; i < nRow; i++)
+            {
+                Console.Write("Eq #{0}: ", i + 1);
+                for (int j = 0; j < nCol; j++)
+                {
+                    if (matrix[i, j] > 0 && j < (nCol - 1) && j > 0)
+                    {
+                        Console.Write("+ {0}*{1}{2} ", matrix[i, j], "X", j + 1);
+                    }
+                    if (matrix[i, j] < 0 && j < (nCol - 1) && j > 0)
+                    {
+                        Console.Write("- {0}*{1}{2} ", Math.Abs(matrix[i, j]), "X", j + 1);
+                    }
+
+                    if (j == 0)
+                    {
+                        Console.Write("{0}*{1}{2} ", matrix[i, j], "X", j + 1);
+                    }
+
+                    if (j == (nCol - 1))
+                    {
+                        Console.Write("= {0}", matrix[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void DisplayResult()
+        {
+            var result = _gausian.BackwardsSubstitution(matrix);
+            Console.WriteLine("\nResult: ");
+            int numX = result.Length;
+            for (int i = 0; i < numX; i++)
+            {
+                Console.WriteLine("X{0}: {1}", i + 1, result[i]);
+            }
+        }
+
+
+    }
+}
---
+++ Matrix.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Equation_System
+{
+    public class Matrix
+    {
+        public int nRow;
+        public int nCol;
+        public double[,] matrix;
+        public Matrix(int newRow, int newCol)
+        {
+            nRow = newRow;
+            nCol = newCol;
+            matrix = new double[nRow, nCol];
+        }
+
+        public double[,] SetLinearEquationMatrix()
+        {           
+            
+            for (int i = 0; i < nRow; i++)
+            {
+                Console.WriteLine("\nEnter coefficient for Eq #{0}: ", i + 1);
+                int input;
+                for (int j = 0; j < nCol; j++)
+                {
+                    Console.Write("Coefficient #{0}: ", j + 1);
+                    while (!int.TryParse(Console.ReadLine(), out input))
+                    {
+                        Console.Write("Please enter cofficient #{0} again: ", j + 1);
+                    }
+                    matrix[i, j] = input;
+                }
+            }
+            return matrix;
+
+        }
+        public void GetMatrix()
+        {
+            for (int i = 0; i < nRow; i++)
+            {
+                for (int j = 0; j < nCol; j++)
+                {
+                    Console.Write("{0}  ",matrix[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
